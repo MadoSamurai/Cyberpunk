416f97edab3ab728505fc7ffa39c3693aad2bf4f
Maxim <ratutuy55@gmail.com>
2026-08-10 22:37:20 +0300

    create shop

--- src/store.js
+++ src/store.js
@@ -1,9 +1,48 @@
+import axios from "axios";
 document.addEventListener('DOMContentLoaded', () => {
   const currentPath = window.location.pathname;
   const header = document.querySelector('.header');
 
-  // Условие выполнится, только если мы НЕ на главной и хедер точно найден в HTML
+  
   if (!currentPath.includes('index.html') && currentPath !== '/' && header) {
     header.style.position = 'static';
   }
 });
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const END_POINT = "/trending/movie/week";
+const API_KEY = "b49261381044063556fe31e38d894915";
+
+const container = document.querySelector('.shop-items-list');
+
+async function serviceShop() {
+  const { data } = await axios(`${BASE_URL}${END_POINT}`,{
+    method: 'GET',
+    params: {
+      api_key: API_KEY
+    }
+  });
+  
+  return data;
+};
+
+serviceShop()
+  .then(data => {
+    console.log(data);  
+    container.innerHTML = createMarcup(data.results);
+  })
+  .catch(error => {
+    alert(error.message);
+  })
+
+function createMarcup(arr) {
+  return arr.map(({ poster_path, original_title, id, vote_count }) => `
+    <li class="shop-items-card">
+      <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}"/>
+      <div class="shop-items-info">
+        <h3 class="shop-items-title" > ${original_title}</h3>
+        <p class="shop-items-price">${vote_count} грн.</p>
+      </div>
+    </li>
+  `).join("");
+  }
